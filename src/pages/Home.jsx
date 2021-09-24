1d8e3c6ad7b7fd7528fd77185d8bb1582c97552c
Molly <momolly1024@gmail.com>
2021-09-24 09:33:55 +0800

    homepage-part2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Newslettter from "../components/Newslettter";
+import Products from "../components/Projucts";
 import Slider from "../components/Slider";
 
 const Home = () => {
@@ -9,6 +13,10 @@ const Home = () => {
             <Announcement />
             <Navbar />
             <Slider />
+            <Categories/>
+            <Products/>
+            <Newslettter/>
+            <Footer/>
         </div>
     );
 };
